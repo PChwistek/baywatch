a844b61caa22acdf66c8ddde6a19f434b3c0c2da
Philip Chwistek <mrsportsnerd@gmail.com>
2019-08-02 00:31:26 -0400

    complete overhaul

--- baywatch.py
+++ baywatch.py
@@ -1,66 +1,96 @@
-import copy 
+## add break once someone has a 0 total...
 
 class LifeguardNode:
   def __init__(self, start, end):
     self.shift = [start, end]
-    self.key = "{}-{}".format(start, end)
-    self.soloStart = start
-    self.soloEnd = end
-    self.soloTotal = self.soloEnd - self.soloStart
-    self.timeConflicts = {} ## key, cost
-
-  def adjustUniqueCoverage(self, otherNodeSoloStart, otherNodeSoloEnd, otherNodeKey):
-    prevTotal = self.soloTotal
-    if self.soloEnd > otherNodeSoloStart and self.soloEnd <= otherNodeSoloEnd:
-      self.soloEnd = otherNodeSoloStart
-    if self.soloStart > otherNodeSoloStart and self.soloStart < otherNodeSoloEnd:
-      self.soloStart = otherNodeSoloEnd
-    self.updateTotal()
-    if prevTotal != self.soloTotal:
-      self.addConflict(otherNodeKey, prevTotal - self.soloTotal)
-    return self.soloTotal
+    self.key = "{}.{}".format(start, end)
+    self.aloneStart = start
+    self.aloneEnd = end
+    self.totalAlone = 0
+    self.conflicts = {}
 
   def updateTotal(self):
-    self.soloTotal = self.soloEnd - self.soloStart
+    self.totalAlone = self.aloneEnd - self.aloneStart if self.aloneEnd - self.aloneStart >= 0 else 0
 
-  def addConflict(self, otherNodeKey, numHoursConflicting):
-    self.timeConflicts[otherNodeKey] = numHoursConflicting
+  def setAloneStart(self, aloneStart, conflictIndex):
+    prevStart = self.aloneStart
+    self.aloneStart = aloneStart
+    self.conflicts[conflictIndex] = aloneStart - prevStart
+
+  def setAloneEnd(self, aloneEnd, conflictIndex):
+    prevEnd = self.aloneEnd
+    self.aloneEnd = aloneEnd
+    self.conflicts[conflictIndex] = prevEnd - aloneEnd
 
   def __repr__(self):
-    return "hours solo:{}".format(self.soloTotal)
+    return "{}:{} - alone {}:{}".format(self.shift[0], self.shift[1], self.aloneStart, self.aloneEnd)
 
-def calculateCoverage(dictOfShifts, lifeguard1, lifeguard2, minKey):
-
-    lg1Start = lifeguard1.soloStart
-    lg1End = lifeguard1.soloEnd
-
-    lg1NewTotal = lifeguard1.adjustUniqueCoverage(lifeguard2.soloStart, lifeguard2.soloEnd, lifeguard2.key)
-    lg2NewTotal = lifeguard2.adjustUniqueCoverage(lg1Start, lg1End, lifeguard1.key)
-
-    return minKey
-
-    
-def readFileInput(pathToFile, dictOfShifts):
+def readFileInput(pathToFile):
   reader = open(pathToFile, "r")
   contents = reader.readlines()
-
-  minKey = None
+  output = []
+  i = 0
   for line in contents:
     editedLine = line.rstrip("\n")
     spaceIndex = editedLine.find(' ')
     if spaceIndex > -1:
-      range = editedLine.split(' ')
-      newNode = LifeguardNode(int(range[0]), int(range[1]))
-      for key in dictOfShifts:
-        minKey = calculateCoverage(dictOfShifts, newNode, dictOfShifts[key], minKey)
-      dictOfShifts[newNode.key] = newNode
+      rangeValues = editedLine.split(' ')
+      newNode = LifeguardNode(int(rangeValues[0]), int(rangeValues[1]))
+      newNode.index = i
+      output.append(newNode)
+      i += 1
 
-  print(minKey)
-  print(dictOfShifts)
   reader.close()
+  output.sort(key=lambda x: x.shift[0])
+  return output
 
+def findMaxHours(inputFile):
+  sortedInput = readFileInput(inputFile)
+  minNode = None
 
-""" invocations... """
+  for index, node in enumerate(sortedInput):
+      next_index = index
+      nextNode = None
 
-dictOfShifts = {}
-readFileInput('./input/1.in', dictOfShifts)
+      if index + 1 < len(sortedInput):
+        next_index += 1
+        nextNode = sortedInput[next_index]
+
+      while nextNode != None:
+        minNode = node if minNode is None else nextNode
+        if node.aloneEnd < nextNode.aloneStart:
+          nextNode = None
+        elif node.aloneEnd > nextNode.aloneStart:
+          temp = nextNode.aloneStart
+          nextNode.setAloneStart(node.aloneEnd, node.key)
+          node.setAloneEnd(temp, nextNode.key)
+
+          node.updateTotal()
+          nextNode.updateTotal()
+          
+          if node.totalAlone < minNode.totalAlone:
+            minNode = node
+          elif nextNode.totalAlone < minNode.totalAlone:
+            minNode = nextNode
+
+          if next_index + 1 < len(sortedInput):
+            nextNode = sortedInput[next_index + 1]
+          else:
+            break
+
+  totalHours = 0
+
+  for index, node in enumerate(sortedInput):
+    if node.key != minNode.key:
+      if node.key in minNode.conflicts:
+        totalHours += minNode.conflicts[node.key]
+      totalHours += node.totalAlone
+
+  print("total hours", totalHours)
+
+# for i in range(10):
+#   findMaxHours('./input/' + str(i + 1) + '.in')
+
+### tests
+findMaxHours('./input/11.in')
+
